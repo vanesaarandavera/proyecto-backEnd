f89646a53038c38e7b74afada0143ce7be429c25
vanesaarandavera <vanesaa715@gmail.com>
2022-08-20 10:24:48 -0300

    inicio de estructura

---
+++ src/main/java/com/vanesaPorfolio/sam/SamApplication.java
@@ -0,0 +1,13 @@
+package com.vanesaPorfolio.sam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SamApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vanesaPorfolio/sam/SamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vanesaPorfolio.sam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
